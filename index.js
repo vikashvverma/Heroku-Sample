c5ac4d684a281960439b353ecd8638fdd1e6d942
Vikash <vikash@programminggeek.in>
2016-01-08 20:20:13 +0530

    Add CORS :+1:

--- index.js
+++ index.js
@@ -9,7 +9,24 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://www.civimechengineering.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.get('/sendemail', function(request, response) {
 
 
