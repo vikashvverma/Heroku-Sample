4d55f4fabdacfe8020f5880320041c438fdf27ae
Vikash <vikash@programminggeek.in>
2016-01-08 20:37:23 +0530

    Allow Emails :envelope:

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', function(request, response) {
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*.*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://www.civimechengineering.com');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
